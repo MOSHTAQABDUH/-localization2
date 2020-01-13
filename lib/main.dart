bddebf06449eeb58bfea1b911eb6871ee8ef1dc7
MOSHTAQABDUH <aklan1992msh@gmail.com>
2020-01-13 09:21:35 +0300

    flutter localization

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'localize_and_translate.dart';
+
+Future<void> main() async {
+  // if your flutter > 1.7.8 :  ensure flutter activated
+  WidgetsFlutterBinding.ensureInitialized();
+
+  LIST_OF_LANGS = ['ar', 'en']; // define languages
+  LANGS_DIR = 'langs/'; // define directory
+  await translator.init(); // intialize
+
+  runApp(
+    LocalizedApp(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+      ],
+      locale: translator.locale,
+      supportedLocales: translator.locals(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  tet(){
+    print(translator.translate('appTitle55'));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text(translator.translate('appTitle')),
+        // centerTitle: true,
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 50),
+            Text(
+              translator.translate('textArea2'),
+              style: TextStyle(fontSize: 35),
+            ),
+            SizedBox(height: 150),
+            OutlineButton(
+              onPressed: () {
+                tet();
+                translator.setNewLanguage(
+                  context,
+                  newLanguage: translator.currentLanguage == 'ar' ? 'en' : 'ar',
+                  remember: true,
+                  restart: true,
+                );
+              },
+              child: Text(translator.translate('textArea1')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
